6ebba5b1b819ff1a3384f72fa49a7a1c937fa360
Jabbernaut <jabbernaut4@gmail.com>
2022-09-01 02:27:03 -0400

    8/31

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
-const { requireAuth } = require('./middleware/authMiddleware');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -15,12 +15,14 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://ksamavati:1234@cluster0.pmnzgch.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://ksamavati:1234@cluster0.pmnzgch.mongodb.net/node-auth?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
-console.log("Listening on port 3000");
+ console.log("Listening at localhost:3000");
+
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
